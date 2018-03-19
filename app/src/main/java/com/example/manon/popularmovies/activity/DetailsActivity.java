7f783b0643a20ddee242498a7e5919faf88f7d8e
Manon Philippot <33070721+maphdev@users.noreply.github.com>
2018-03-19 15:54:43 +0100

    Favorite stars in details activity.

--- app/src/main/java/com/example/manon/popularmovies/activity/DetailsActivity.java
+++ app/src/main/java/com/example/manon/popularmovies/activity/DetailsActivity.java
@@ -1,7 +1,10 @@
 package com.example.manon.popularmovies.activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,14 +14,18 @@ import com.example.manon.popularmovies.utils.NetworkUtils;
 import com.squareup.picasso.Picasso;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private Boolean favorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-
+        
         Movie movie = getIntent().getParcelableExtra("movie_parcel");
         setTitle("Movie details");
 
@@ -39,7 +46,40 @@ public class DetailsActivity extends AppCompatActivity {
         TextView releaseDate = findViewById(R.id.release_date);
         releaseDate.setText(movie.getReleaseDate());
 
-        TextView voteAverage = findViewById(R.id.vote_average);
-        voteAverage.setText(Double.toString(movie.getVoteAverage())+"/10");
+        // display vote average stars
+        List<ImageView> listStars = new ArrayList<>();
+        listStars.add((ImageView)findViewById(R.id.star1));
+        listStars.add((ImageView)findViewById(R.id.star2));
+        listStars.add((ImageView)findViewById(R.id.star3));
+        listStars.add((ImageView)findViewById(R.id.star4));
+        listStars.add((ImageView)findViewById(R.id.star5));
+        double voteAverage = Math.round(movie.getVoteAverage());
+        int starsIndice = 0;
+        while(voteAverage >=2) {
+            voteAverage-=2;
+            listStars.get(starsIndice).setBackground(getResources().getDrawable(R.drawable.ic_star_black_24dp));
+            starsIndice += 1;
+        }
+        if (voteAverage >= 1) {
+            listStars.get(starsIndice).setBackground(getResources().getDrawable(R.drawable.ic_star_half_black_24dp));
+        }
+
+        // display favorite
+        favorite = false;
+        final FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                favorite = !favorite;
+                if (favorite)
+                    floatingActionButton.setImageResource(R.drawable.ic_favorite_black_24dp);
+                else
+                    floatingActionButton.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+            }
+        });
+    }
+
+    public static double roundToHalfPoint(double d) {
+        return Math.round(d * 2) / 2.0;
     }
 }
